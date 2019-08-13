e062302ae8fc80527cc2057e11d05d52e61aa171
Lambda_School_Loaner_145 <lambda_school_loaner_145@Lambda-School-Loaner-145s-MacBook-Pro.local>
2019-08-13 14:57:56 -0700

    debug of number buttons

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -3,14 +3,11 @@ import NumberButton from './NumberButton'
 import { numbers } from '../../../data'
 
 const Numbers = () => {
-  const {NumberButton, setNumberButton} = useState(numbers)
+  const {buttonNumbers, setButtonNumbers} = useState(numbers)
   
   return (
     <div>
-      {/* STEP 3 - Use .map() to iterate over your array data and return a button
-       component matching the name on the provided file. Pass
-       it any props needed by the child component*/}
-       <NumberButton />
+      {buttonNumbers.map(number => <NumberButton text={number} />)}
     </div>
   );
 };
