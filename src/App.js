867d1cbe546709f8e7eb9ecb9d3819eeb8d89f1a
Lambda_School_Loaner_145 <lambda_school_loaner_145@Lambda-School-Loaner-145s-MacBook-Pro.local>
2019-08-13 16:57:45 -0700

    special char buttons added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Logo from './components/DisplayComponents/Logo';
 import Display from './components/DisplayComponents/Display';
 import Numbers from './components/ButtonComponents/NumberButtons/Numbers'
 import Operators from './components/ButtonComponents/OperatorButtons/Operators'
+import Specials from './components/ButtonComponents/SpecialButtons/Specials'
 // import OperatorButtons from './components/ButtonComponents/OperatorButtons/OperatorButton'
 // import Button
 
@@ -22,6 +23,7 @@ function App() {
         <Display number={0} />
         <Numbers />
         <Operators />
+        <Specials />
       </div>
     </div>
   );
